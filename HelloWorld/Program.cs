f7ca7f6f8d7365d934c431f97304fe4b25c9fc9b
Noahkiq <noahkiqgaming@gmail.com>
2016-08-18 17:36:21 -0400

    added all the other files
    thanks for not adding them visual studio <3

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("say something");
+            string s = Console.ReadLine();
+            if (s == "something")
+            {
+                Console.WriteLine("k");
+            }
+            else
+            {
+                Console.WriteLine("you didnt say \"something\" D-");
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                File.WriteAllLines(path + "\\todo.txt", new string[] { "troll someone on 4chan", "make a dank maymay", "do another dumb thing" });
+            }
+            Console.ReadKey();
+            Console.WriteLine("- shush child no talking");
+            Console.ReadKey();
+            Console.WriteLine("- shhh");
+            Console.ReadKey();
+            Console.WriteLine("- no fuck off");
+        }
+    }
+}
