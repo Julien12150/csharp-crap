0a125cdc2641a50da4f4969a430d25994fda7da8
Julien12150 <julienfillol2@gmail.com>
2016-08-19 12:46:53 +0200

    improved thing, it will get triggered in a loop if you say something offending

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -7,16 +7,29 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
             Console.WriteLine("say something");
             string s = Console.ReadLine();
             if (s == "something")
             {
                 Console.WriteLine("k");
             }
+            else if(s == "fuck you")
+            {
+                File.WriteAllLines(path + "\\todo (dont look mam).txt", new string[] {
+                    "TODO LIST (mam plz close this)",
+                    "",
+                    "-kill mom",
+                    "-kill dad",
+                    "-fuck mom",
+                    "-kill self"});
+                Console.WriteLine("no fuck you you little shit");
+                while (true)
+                    Console.WriteLine("you're ugly fuck you");
+            }
             else
             {
                 Console.WriteLine("you didnt say \"something\" also what's that todo file on your desktop");
-                string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
                 File.WriteAllLines(path + "\\epik todo list.txt", new string[] {
                     "EPIK TODO LIST (amazing) ;)",
                     "",
